bcf0b7fa436d4a2e0ad3d763ba710ddde373acc2
rat soni <andryll.silva@outlook.com>
2023-04-03 20:41:54 -0300

    adicionado events e style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,51 +2,62 @@
 fetch("src/scripts/objects/products.json")
     .then(response => response.json())
     .then(data => {
+
+        // Seleciona os elementos HTML necessários
         const products = data.products;
         const produtosSection = document.querySelector(".product-container");
         const entregaGratisCheckbox = document.getElementById('delivery');
         const promocaoCheckbox = document.getElementById("promotion");
-        console.log(produtosSection)
 
-        function inicializarLoja() {
+        // Função para exibir os produtos com base nos filtros selecionados
+        function startShop() {
+            // Filtra os produtos com base nos filtros selecionados
+            produtosSection.innerHTML = "";
             const produtosFiltrados = products.filter(produto => {
                 let atendeFiltros = true;
-                if (entregaGratisCheckbox.checked && !produto.entrega_gratis) {
+                if (entregaGratisCheckbox.checked && !produto.delivery) {
                     atendeFiltros = false;
                 }
-                if (promocaoCheckbox.checked && !produto.promocao) {
+                if (promocaoCheckbox.checked && !produto.promotion) {
                     atendeFiltros = false;
                 }
 
                 return atendeFiltros;
             });
 
-
+            // Exibe os produtos filtrados na seção de produtos
             produtosFiltrados.forEach(produto => {
                 const produtoDiv = document.createElement("div");
                 produtoDiv.classList.add("product");
 
+                const linkProduct = document.createElement("a")
+                linkProduct.classList.add("link-product")
+                linkProduct.setAttribute("href","#")
                 const imagem = document.createElement("img");
                 imagem.src = produto.image;
-                produtoDiv.appendChild(imagem);
+                linkProduct.appendChild(imagem)
+                produtoDiv.appendChild(linkProduct);
 
                 const nome = document.createElement("h2");
                 nome.textContent = produto.name;
-                produtoDiv.appendChild(nome);
-
-                const descricao = document.createElement("p");
-                descricao.textContent = produto.descricao;
-                produtoDiv.appendChild(descricao);
+                linkProduct.appendChild(nome)
 
                 const price = document.createElement("p");
                 price.classList.add("price");
+                price.setAttribute("id","discount")
                 price.textContent = `R$ ${produto.price.toFixed(2)}`;
                 if (produto.promotion) {
-                    price.classList.add("promocao");
+                    const divPromotion = document.createElement("div")
+                    divPromotion.classList.add("promotion")
                     const oldPrice = document.createElement("p")
-                    oldPrice.classList.add("promotion")
+                    oldPrice.setAttribute("id","oldPrice")
+                    const discount = document.createElement("p")
+                    discount.setAttribute("id","discount")
                     oldPrice.textContent = `R$ ${produto.oldPrice.toFixed(2)}`
-                    produtoDiv.appendChild(oldPrice)
+                    discount.textContent = `30%`
+                    divPromotion.appendChild(oldPrice)
+                    divPromotion.appendChild(discount)
+                    produtoDiv.appendChild(divPromotion)
                 }
                 produtoDiv.appendChild(price);
 
@@ -54,9 +65,22 @@ fetch("src/scripts/objects/products.json")
                     const freeDelivery = document.createElement("p");
                     freeDelivery.textContent = "Entrega Grátis";
                     freeDelivery.classList.add("free-delivery");
+                    freeDelivery.setAttribute("id","discount")
                     produtoDiv.appendChild(freeDelivery);
                 }
 
+                const description = document.createElement("p");
+                description.setAttribute("id","description")
+                description.textContent = produto.description;
+                produtoDiv.appendChild(description);
+
+                const divBuyBtn = document.createElement("div")
+                divBuyBtn.classList.add("div-buyBtn")
+                const buyBtn = document.createElement("button")
+                buyBtn.classList.add("buy-btn")
+                buyBtn.textContent = "Adicionar ao Carrinho"
+                divBuyBtn.appendChild(buyBtn)
+                produtoDiv.appendChild(divBuyBtn)
                 produtosSection.appendChild(produtoDiv);
             })
 
@@ -70,6 +94,9 @@ fetch("src/scripts/objects/products.json")
 
             
         }
+        startShop()
 
-        inicializarLoja()
+         // Adiciona eventos para atualizar os produtos quando os filtros são alterados
+        entregaGratisCheckbox.addEventListener("change", startShop);
+        promocaoCheckbox.addEventListener("change", startShop);
     })
